17bb1e3cae192de01990b87717060fa926f31149
Rakshitha S <62503907+rakshithamatthuga@users.noreply.github.com>
2020-11-27 15:32:21 +0530

    Add files via upload

---
+++ src/session.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dell
+ */
+public class session {
+public static String user=null;
+public void setusername(String name){
+    user=name;
+}
+public String getusername(){
+    return user;
+}
+    
+    
+}
